60cf0f4a53923e20b1a839ea53528de084acb576
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-07-11 23:01:28 +0800

    add demo image

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 const juxtaposeDemo = document.getElementsByClassName("juxtapose-demo")[0];
+const demoImageList=['https://i.imgur.com/LLcYT76.png','https://i.imgur.com/eB8uL3y.png','https://i.imgur.com/M7APpgf.png','https://i.imgur.com/ak1Donj.png']
+const demoId=Math.floor(Math.random() * 4);
+
 juxtaposeDemo.innerHTML = "";
 // juxtaposeDemo.classList.add("animate__fadeIn");
     slider = new juxtapose.JXSlider('.juxtapose-demo',
         [
             {
-                src: `https://i.imgur.com/LLcYT76.png`,
+                src: demoImageList[demoId],
                 label: 'Before',
                 // credit: 'Image Credit'
             },
             {
-                src: `https://i.imgur.com/LLcYT76.png`,
+                src: demoImageList[demoId],
                 label: 'After',
                 // credit: "Image Credit"
             }
